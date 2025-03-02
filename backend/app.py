4c1b287f18b2f6fadb2961e42a9887a152f5a291
Abhay Pratap Singh <abhaypratapsingh481833@gmail.com>
2025-03-02 12:29:39 +0530

    backend adding to render since python anywhere is not supporting the wbesocket

--- backend/app.py
+++ backend/app.py
@@ -1,56 +1,44 @@
-from flask_socketio import SocketIO, emit, rooms
-import jwt
+from flask_socketio import SocketIO, emit
+
 import datetime
-
-
-###### for mails ######
-# import os
-import smtplib
-from email.mime.text import MIMEText
-
 
 import jwt as pyJWT
 
-from flask import Flask, request, jsonify, make_response
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 from flask_sqlalchemy import SQLAlchemy
 
-from functools import wraps
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, create_refresh_token, get_jwt
-from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
-from datetime import timedelta
+from flask_jwt_extended import JWTManager
 
-from matplotlib.style import available
-from sqlalchemy import text
-import datetime
 
-from sympy import limit
-app = Flask(__name__)
-from flask import Flask, request, jsonify
-from flask_socketio import SocketIO, emit
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from flask_cors import CORS
+
 
 app = Flask(__name__)
+# import random
 
-# Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/emoji_db?charset=utf8mb4'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_POOL_SIZE'] = 10  
-app.config['SQLALCHEMY_POOL_TIMEOUT'] = 30  
-app.config['SQLALCHEMY_POOL_RECYCLE'] = 280 
-app.config['SQLALCHEMY_MAX_OVERFLOW'] = 5
-app.config['SECRET_KEY'] = 'your_secret_key'
-db = SQLAlchemy(app)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://AndroidAbhayPrat:FAfjals2rlqs9iq2ljkas02j@AndroidAbhayPratapSingh.mysql.pythonanywhere-services.com:3306/AndroidAbhayPrat$temp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tracking
+app.config['SQLALCHEMY_POOL_SIZE'] = 10  # Number of connections to maintain in the pool
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 30  # How long to wait for a connection
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 280  # Recycle connections after this many seconds
+app.config['SQLALCHEMY_MAX_OVERFLOW'] = 5  # Allow up to 5 additional connections beyond the pool size
+
+# this is needed to be change
+app.config['JWT_SECRET_KEY'] = 'supersecretkeyq23rq123'
+app.config['SECRET_KEY'] = "aksdhjnvocasj234aslfk293r"
+
+# token generation and checking
 jwt = JWTManager(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
+# this is for connecting data base
+db = SQLAlchemy(app)
+#this is for enabling the cors
 CORS(app, supports_credentials=True)
+
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 active_Users = {}
@@ -119,17 +107,17 @@ def check():
 def sign_in_request():
     if request.method == 'OPTIONS':
         return '', 204
-    
+
     valid, data = getData()
-    
+
     if not valid :
         return data
-    
+
     if not haveRequiredData(data, "name","password","phoneNo","email",accurate = True) :
         return jsonify({"error":"Required Data's Items Not exact match!"}),400
-    
+
     exist, message = userExist(data['email'],data['password'])
-    if exist:    
+    if exist:
         return jsonify({"error":"user Exist"}),401
     try:
         query = text("INSERT INTO users (name, password, id , phoneNumber) VALUES (:name, :password, :email, :phoneNo)")
@@ -144,26 +132,26 @@ def sign_in_request():
 def log_in_request():
     if request.method == 'OPTIONS':
         return '', 204
-    
+
     valid, data = getData()
-    
+
     if not valid :
         return data
-    
+
     if not haveRequiredData(data, "id","pwd",accurate = True) :
         return jsonify({"error":"Required Data's Items Not exact match!"}),400
-    
+
     exist, message = userExist(data['id'],data['pwd'])
-    if not exist:    
+    if not exist:
         return message
     try:
         query = text("select id, password, phoneNumber,name  from users where id = :id and password = :password")
         result = db.session.execute(query, {'id': data["id"],'password': data["pwd"]}).fetchall()
         if len(result) == 0 :
             return ({"error":"User Doesn't Exits"}),401
-        
+
         token = create_token(data['id'])
-        
+
         data = {
             "name":result[0][3],
             "id":result[0][0],
@@ -172,7 +160,7 @@ def log_in_request():
         return jsonify({"data":data,"token":token}),200
 
     except Exception as e:
-        return jsonify({"error":"Internal Server Error"}),500
+        return jsonify({"error":f"Internal Server Error : {e}"}),500
 
 @app.route("/find-friend",methods=["POST","OPTIONS"])
 def findFriend():
@@ -183,15 +171,15 @@ def findFriend():
         token = token.replace('Bearer ', '')  # Removes 'Bearer ' prefix if it exists
     else:
         return jsonify({"error": "No token provided"}), 401
-    
+
     valid, data = getData()
-    
+
     if not valid :
         return data
-    
+
     if not haveRequiredData(data,"id",accurate = True) :
         return jsonify({"error":"Required Data's Items Not exact match!"}),400
-    
+
     userData,valid = verify_token(token)
     if not valid:
         return jsonify({"error":"no token found"}),401
@@ -199,7 +187,7 @@ def findFriend():
         id = userData['userId']
         query = text("select id, password, phoneNumber,name  from users where id = :id ")
         result = db.session.execute(query, {'id': data['id']}).fetchall()
-        
+
         if len(result) >0 :
             data = {
                 "name": result[0][3],
@@ -217,15 +205,15 @@ def findFriend():
 def addFriend():
     if request.method == 'OPTIONS':
         return '', 204
-        
+
     valid, data = getData()
-    
+
     if not valid :
         return data
-    
+
     if not haveRequiredData(data,"id","fid",accurate = True) :
         return jsonify({"error":"Required Data's Items Not exact match!"}),400
-    
+
     try:
         id,fid = data['id'] ,data['fid']
         query = text("select id1,id2 from friend where (id1 = :id1 and id2 = :id2) or (id1 = :id2 and id2 = :id1) ")
@@ -247,22 +235,22 @@ def addFriend():
 def set_seen_all():
     if request.method == 'OPTIONS':
         return '', 204
-    
+
     token = request.headers.get('Authorization')
     if token:
-        token = token.replace('Bearer ', '')  # Removes 'Bearer ' prefix if it exists
+        token = token.replace('Bearer ', '')
     else:
         return jsonify({"error": "No token provided"}), 401
     valid, data = getData()
-    
+
     if not valid :
         return data
-    
+
     if not haveRequiredData(data,"receiverId",accurate = True) :
         return jsonify({"error":"Required Data's Items Not exact match!"}),400
-    
+
     exist, message = userExist(data['senderId'])
-    if not exist:    
+    if not exist:
         return message
     userData,valid = verify_token(token)
     if not valid:
@@ -289,25 +277,30 @@ def getAllData(id):
 
     data = []
     for each in result:
+
         friend = each[0] if str(each[0]) != str(id) else each[1]
         friendData = db.session.execute(userQuery, {"id": friend}).fetchone()
         chatsData = db.session.execute(chatQuery, {"id": id, "id2": friend}).fetchall()
         messages=[]
+
+        readAll = text("select 1 from chats where senderId = :id and receiveTime is not null")
+        readAllMes = db.session.execute(readAll,{"id":friend} ).fetchall()
+
         for every in chatsData:
             messages.append(
                 {
                     "message": every[0],
                     "type": "send" if every[1] == id else "receive",
-                    "time": int(every[3].timestamp())*1000
+                    "time": int(every[3].timestamp())*1000,
                 }
             )
 
-        try: 
+        try:
             data.append({"id": friendData[0], "name": friendData[2], "phNo": friendData[1], "message": messages})
         except:
             pass
     return data
-    
+
 
 @socketio.on('connect')
 def handle_connect():
@@ -318,9 +311,9 @@ def handle_connect():
             return
 
         active_Users[userId] = request.sid
-        
+
         data = getAllData(userId)
-        
+
         emit('message', {'data': data, 'type':'users'})
 
     except Exception as e:
@@ -329,14 +322,25 @@ def handle_connect():
 
 @socketio.on('message')
 def handle_message(data):
+    print("on message: ")
+    print(data)
     try:
         type = data['type']
-        mainId = data['id']
         if (type == "messageSend"):
             query = text("INSERT INTO chats (message, senderId, receiverId) VALUES (:mess, :sendId, :revId)")
             db.session.execute(query, {'mess': data["data"]['message'], 'sendId': data['data']["senderId"], "revId": data["data"]["receiverId"]})
             db.session.commit()
-            
+            recevier =  data['data']['receiverId']
+            newData = {
+                "message" : data['data']['message'],
+                "time" : int(datetime.datetime.now().timestamp()*1000)
+            }
+            if (recevier in active_Users):
+                emit('message', {'data':newData,"sender":data['data']['senderId'], 'type':'newMessage'}, room=[active_Users[recevier]] )
+                print("user is active",recevier)
+
+            emit('message', {'data':newData, "receiver":recevier, 'type':'delieverMessage'} )
+
         if type == "addFriend":
             id = data['data']['id']
             query = text("select id from users where id = :id ")
@@ -348,9 +352,7 @@ def handle_message(data):
                     query = text("insert into friend(id1, id2) values(:id1, :id2) ")
                     db.session.execute(query, {'id1': id, "id2":data['data']['fid']})
                     db.session.commit()
-                    
-        data = getAllData(mainId)
-        emit('message', {'data': data, 'type':'users'})
+
 
     except Exception as e:
         print(f"Error: {e}")
@@ -364,7 +366,3 @@ def on_disconnect():
             break
     emit('message', {'data': 'You have disconnected.'})
 
- 
-
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
